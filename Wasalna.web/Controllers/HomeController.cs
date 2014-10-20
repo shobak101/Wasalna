0033d50689176ca0f6dba16342bd2ffa917da94a
Hosam Shobak <hosam.shobak@gmail.com>
2014-10-19 23:52:51 -0700

    Wasalna First Commit- empty MVC 4 app

---
+++ Wasalna.web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wasalna.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Wasalna Page";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Wasalna";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contacts";
+
+            return View();
+        }
+    }
+}
